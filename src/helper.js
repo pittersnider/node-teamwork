6bcbab09d9b0f944b90971a3c72f8a8aa05e1f4b
Pitter <snider@degiant.com.br>
2018-02-16 21:12:44 -0200

    [+] Created new Helper

---
+++ src/helper.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const moment = require('moment');
+
+const regex = {
+    convertsTimestring: () => /^([0-9]+\.[0-9]+)$/ig
+};
+
+class Helper {
+
+    /**
+     * Returns the current date in TeamWork format.
+     */
+    static date() {
+        return moment().format('YYYYMMDD');
+    }
+
+    /**
+     * Returns the current time in TeamWork format.
+     */
+    static time() {
+        return moment().format('HH:mm');
+    }
+
+    /**
+     * Converts decimal number to hour and minutes,
+     *  then subtract from the current moment or from
+     *  the specified 'from' moment.
+     * 
+     * FIXME: Separar em duas funções.
+     * 
+     */
+    static convertsTimestring({ string, from = moment() }) {
+
+        const time = { hour: 0, minutes: 0 };
+        let pattern = regex.convertsTimestring();
+
+        if ((pattern = pattern.exec(string))) {
+
+            let timeInMinutes = Number.parseFloat(pattern[1]) * 60;
+            time.minutes = timeInMinutes % 60;
+            time.hour = (timeInMinutes - time.minutes) / 60;
+
+        }
+
+        return from.subtract(time.hour, 'h').subtract(time.minutes, 'm').format('HH:mm');
+
+    }
+
+}
+
+module.exports = Helper;
